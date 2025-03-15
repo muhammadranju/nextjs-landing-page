f84355b8bd8aea2e6ca5f5fab96bee1af21a6297
Muhammad Ranju <mdranju23@gmail.com>
2025-03-15 16:13:57 +0600

    feat: Add Header component with responsive navigation and theme toggle

---
+++ src/components/header.tsx
@@ -0,0 +1,82 @@
+"use client";
+import { Menu, X } from "lucide-react";
+import React, { useState } from "react";
+import { Logo } from "./logo";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { ModeToggle } from "./ui/theme-button";
+
+const menuItems = [
+  { name: "Home", href: "/" },
+  { name: "Features", href: "/features" },
+  { name: "Pricing", href: "/pricing" },
+  { name: "Contact", href: "/contact" },
+];
+
+const Header = () => {
+  const [menuState, setMenuState] = useState(false);
+  return (
+    <header className="sticky top-0 z-50">
+      <nav
+        data-state={menuState && "active"}
+        className="fixed z-20 w-full border-b border-dashed bg-white backdrop-blur md:relative dark:bg-zinc-950/50 lg:dark:bg-transparent"
+      >
+        <div className="m-auto max-w-7xl px-6">
+          <div className="flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
+            <div className="flex w-full justify-between lg:w-auto">
+              <Link
+                href="/"
+                aria-label="home"
+                className="flex items-center space-x-2"
+              >
+                <Logo />
+              </Link>
+
+              <button
+                onClick={() => setMenuState(!menuState)}
+                aria-label={menuState == true ? "Close Menu" : "Open Menu"}
+                className="relative z-20 -m-2.5 -mr-4 block cursor-pointer p-2.5 lg:hidden"
+              >
+                <Menu className="in-data-[state=active]:rotate-180 in-data-[state=active]:scale-0 in-data-[state=active]:opacity-0 m-auto size-6 duration-200" />
+                <X className="in-data-[state=active]:rotate-0 in-data-[state=active]:scale-100 in-data-[state=active]:opacity-100 absolute inset-0 m-auto size-6 -rotate-180 scale-0 opacity-0 duration-200" />
+              </button>
+            </div>
+
+            <div className="bg-background in-data-[state=active]:block lg:in-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent">
+              <div className="lg:pr-4">
+                <ul className="space-y-6 text-base lg:flex lg:gap-8 lg:space-y-0 lg:text-sm">
+                  {menuItems.map((item, index) => (
+                    <li key={index}>
+                      <Link
+                        href={item.href}
+                        className="text-muted-foreground hover:text-accent-foreground block duration-150"
+                      >
+                        <span>{item.name}</span>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit lg:border-l lg:pl-6">
+                <Button asChild variant="outline" size="sm">
+                  <Link href="/auth/login">
+                    <span>Login</span>
+                  </Link>
+                </Button>
+                <Button asChild size="sm">
+                  <Link href="/auth/signup">
+                    <span>Signup</span>
+                  </Link>
+                </Button>
+                <ModeToggle />
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
