5228fe5511489676c1ccb55c8407aba15c42e51a
Muhammad Ranju <mdranju23@gmail.com>
2025-03-15 16:13:07 +0600

    Add features page with header and components for landing page

---
+++ src/app/features/page.tsx
@@ -0,0 +1,16 @@
+import Features from "@/components/features-1";
+import FeaturesSection7 from "@/components/features-7";
+import Header from "@/components/header";
+
+const FeaturesPage = () => {
+  return (
+    <>
+      <title>Features | Next.js Landing Page</title>
+      <Header />
+      <FeaturesSection7 />
+      <Features />
+    </>
+  );
+};
+
+export default FeaturesPage;
