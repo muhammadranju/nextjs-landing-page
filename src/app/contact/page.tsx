7275811367343123664afb3581207c98fa976dbf
Muhammad Ranju <mdranju23@gmail.com>
2025-03-15 16:13:02 +0600

    Add contact page with header and content sections

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,17 @@
+import ContentSection from "@/components/content-2";
+import ContentSection5 from "@/components/content-5";
+import Header from "@/components/header";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <title>Contact | Next.js Landing Page</title>
+      <Header />
+      <ContentSection />
+      <ContentSection5 />
+    </>
+  );
+};
+
+export default page;
