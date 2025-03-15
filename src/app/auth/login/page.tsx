ec29c62141df1187a97ac78ac92a55e71b656646
Muhammad Ranju <mdranju23@gmail.com>
2025-03-15 16:12:56 +0600

    Add login and signup pages for authentication

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginPage from "@/components/login";
+
+const Login = () => {
+  return (
+    <>
+      <title>Login | Next.js Landing Page</title>
+      <LoginPage />
+    </>
+  );
+};
+
+export default Login;
---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,13 @@
+import SignupPage from "@/components/sign-up";
+import React from "react";
+
+const Signup = () => {
+  return (
+    <>
+      <title>Signup | Next.js Landing Page</title>
+      <SignupPage />
+    </>
+  );
+};
+
+export default Signup;
