7078322c3a6438812dde062168c8e31e4784463c
Muhammad Ranju <mdranju23@gmail.com>
2025-03-15 16:13:17 +0600

    feat: Add pricing page with header and pricing components

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/header";
+import Pricing1 from "@/components/pricing1";
+import Pricing from "@/components/pricing";
+
+const page = () => {
+  return (
+    <>
+      <title>Pricing | Next.js Landing Page</title>
+      <Header />
+      <Pricing1 />
+      <Pricing />
+    </>
+  );
+};
+
+export default page;
